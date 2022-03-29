1fcb140b2cabcc38a94e67a9bef90d9b444d49e4
Laken Harville <rlakenharville@gmail.com>
2022-03-29 17:09:00 -0500

    Initial commit

---
+++ BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+    {
+        private double balance;
+        
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"Depositing {amount, 0:c} to your account.");
+            balance += amount; 
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+
+    }
+        
+    
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount ba = new BankAccount();
+
+            Console.WriteLine($"Please make a deposit.");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            ba.Deposit(amountToDeposit);
+
+            double userBalance = ba.GetBalance();
+
+            Console.WriteLine($"Balance: {userBalance,0:c}");
+
+        }
+    }
+}
